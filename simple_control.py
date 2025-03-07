78eea688f246f32180f4d2183815c0f936d97589
jasonzhang <embeddedtec@outlook.com>
2025-03-07 20:19:53 +0800

    补全功能，get_operate_mode 有bug，退出比较耗时

--- simple_control.py
+++ simple_control.py
@@ -7,21 +7,71 @@ def main():
     
     if hub:
         print(f"Connected to {hub.name}")
-        while True:
-            power_status = hub.get_channel_power_status(1,2,3,4)
-            if power_status:
-                print(f"power_status ch1: {power_status.get(1)}, ch2: {power_status.get(2)}, ch3: {power_status.get(3)}, ch4: {power_status.get(4)}")
+        # get firmware version
+        firmware_version = hub.get_firmware_version()
+        print(f"firmware_version: {firmware_version}")
 
-            # Flip the power state of each channel
-            if power_status:
-                for ch in range(1, 5):
-                    current_state = power_status.get(ch, 0)
-                    new_state = 0 if current_state else 1
-                    hub.set_channel_power_status(ch, state=new_state)
-                    if hub.debug:
-                        print(f"Set channel {ch} to {'ON' if new_state else 'OFF'}")
+        # get hardware version
+        hardware_version = hub.get_hardware_version()
+        print(f"hardware_version: {hardware_version}")
+        
+        # get operation mode
+        operation_mode = hub.get_operate_mode()
+        print(f"operation_mode: {operation_mode}")
 
-            time.sleep(0.01)
+        # get button status
+        button_status = hub.get_button_control()
+        print(f"button_status: {button_status}")
+
+        try:
+            while True:
+                
+                # power_status = hub.get_channel_power_status(1,2,3,4)
+                # if power_status:
+                #     print(f"power_status ch1: {power_status.get(1)}, ch2: {power_status.get(2)}, ch3: {power_status.get(3)}, ch4: {power_status.get(4)}")
+
+                # # Flip the power state of each channel
+                # if power_status:
+                #     for ch in range(1, 5):
+                #         current_state = power_status.get(ch, 0)
+                #         new_state = 0 if current_state else 1
+                #         hub.set_channel_power_status(ch, state=new_state)
+                #         if hub.debug:
+                #             print(f"Set channel {ch} to {'ON' if new_state else 'OFF'}")
+
+                # dataline_status = hub.get_channel_dataline_status(1,2,3,4)
+                # if dataline_status:
+                #     print(f"dataline_status ch1: {dataline_status.get(1)}, ch2: {dataline_status.get(2)}, ch3: {dataline_status.get(3)}, ch4: {dataline_status.get(4)}")
+                # else:
+                #     print("dataline_status is None")
+
+                # voltage_readings = {}
+                # for ch in [1, 2, 3, 4]:
+                #     reading = hub.get_channel_voltage(ch)
+                #     # reading 可能是 None，需做相应判断
+                #     voltage_readings[ch] = reading
+
+                # print("Voltage readings:")
+                # for ch, val in voltage_readings.items():
+                #     print(f"  ch{ch}: {val}")
+                
+                current_readings = {}
+                for ch in [1, 2, 3, 4]:
+                    reading = hub.get_channel_current(ch)
+                    current_readings[ch] = reading
+
+                print("Current readings:")
+                for ch, val in current_readings.items():
+                    print(f"  ch{ch}: {val}")
+
+
+                time.sleep(0.01)
+
+        except KeyboardInterrupt:
+            print("Exiting program...")
+            hub.disconnect()
+            print("program exit.")
+
     else:
         print("No Smart USB Hub found.")
 
